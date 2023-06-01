31d200f19fd1b945759b55198005a6b1734563ba
311609 <94932711+311609@users.noreply.github.com>
2023-06-01 17:07:15 -0600

    se agrega formulario

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -4,20 +4,34 @@ import { v4 as uuidv4 } from 'uuid';
 
 function TaskForm(props) {
   const [input, setInput] = useState('');
+  const [descripcion, setDescripcion] = useState('');
 
   const manejarCambio = e => {
     setInput(e.target.value);
   }
 
+  const manejarCambioDescripcion = e => {
+    setDescripcion(e.target.value);
+  }
+
   const manejarEnvio = e => {
     e.preventDefault();
+
+    if (input.length < 3 && descripcion.length > 0) {
+      alert('El nombre de la tarea debe tener al menos 3 caracteres.');
+      return;
+    }
+
     const tareaNueva = {
       id: uuidv4(),
       texto: input,
+      descripcion: descripcion,
       completada: false
     }
     props.onSubmit(tareaNueva);
-    setInput(''); 
+    
+    setInput('');
+    setDescripcion('');
   }
 
   return (
@@ -29,6 +43,14 @@ function TaskForm(props) {
         name='texto'
         value={input} 
         onChange={manejarCambio}
+      />
+      <input
+        className='tarea-input'
+        type='text'
+        placeholder='Ingresar descripción'
+        name='descripcion'
+        value={descripcion} 
+        onChange={manejarCambioDescripcion}
       />
       <button className='tarea-boton'>
         Agregar
